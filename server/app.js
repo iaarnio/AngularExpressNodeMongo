2adeb0b086a2ac0d8521c5e641e8a3f2ffccca6d
Ilari Aarnio <jia@iki.fi>
2015-04-16 11:40:18 +0300

    enchance readability of var declarations

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var logger = require('morgan');
-var path = require('path');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 // Setup server
 var app = express();
-var server = require('http').createServer(app);
+var server = http.createServer(app);
 var port = 3000;
 
 app.use(express.static('/client'));
