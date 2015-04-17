d950db31f3d30ebd83277afb800e49c0b4ba3bd4
Ilari Aarnio <jia@iki.fi>
2015-04-17 20:45:26 +0300

    remove debug code

--- client/app/book/book.controller.js
+++ client/app/book/book.controller.js
@@ -29,40 +29,7 @@ function BookController(bookDataService, $log) {
   }
 
   function addBook() {
-    $log.info('add: :"' + vm.newBook + '"');
-    $log.info('add: hipsut :"' + '' + '"');
-
-    var a1 = {};
-    if (a1 === '') { $log.debug('hipsu') }
-    if (a1 === {}) { $log.debug('{}') }
-    if (a1 === []) { $log.debug('[]') }
-    if (a1 === null) { $log.debug('null') }
-
-    var a2 = {};
-    a2.foo = '';
-    a2.bar = '';
-    if (a2 === '') { $log.debug('a2 hipsu') }
-    if (a2 === {}) { $log.debug('a2 {}') }
-    if (a2 === []) { $log.debug('a2 []') }
-    if (a2 === null) { $log.debug('a2 null') }
-
-    vm.a3 = {};
-    vm.a3.foo = '';
-    vm.a3.bar = '';
-    if (vm.a3 === '') { $log.debug('a3 hipsu') }
-    if (vm.a3 === {}) { $log.debug('a3 {}') }
-    if (vm.a3 === []) { $log.debug('a3 []') }
-    if (vm.a3 === null) { $log.debug('a3 null') }
-
-    $log.debug('nb=' + vm.newBook);
-    $log.debug('nb.t=' + vm.newBook.title);
-
-    if (vm.newBook.title === '') {
-      $log.debug('TITLE haara');
-    }
-
     if (vm.newBook === '') {
-      $log.debug('RETURN haara');
       return;
     }
     bookDataService.addBook(vm.newBook)
